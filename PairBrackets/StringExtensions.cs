a8f9ae8e8cf7587e4dba7e4bd1793cdc69d39aad
glock <glockajulia@gmail.com>
2025-06-02 22:36:55 +0200

    solution v1

--- PairBrackets/StringExtensions.cs
+++ PairBrackets/StringExtensions.cs
@@ -1,43 +1,117 @@
-namespace PairBrackets;
+using System;
+using System.Collections.Generic;
 
-/// <summary>
-/// Provides extension methods for working with strings and bracket pairs.
-/// </summary>
-public static class StringExtensions
+namespace PairBrackets
 {
-    /// <summary>
-    /// Returns the number of bracket pairs in the <see cref="text"/>.
-    /// </summary>
-    /// <param name="text">The source text.</param>
-    /// <returns>The number of bracket pairs in the <see cref="text"/>.</returns>
-    public static int CountBracketPairs(this string text)
+    [Flags]
+    public enum BracketType
     {
-        // TODO #1. Analyze the method unit tests and add the method implementation.
-        throw new NotImplementedException();
+        Round = 1,
+        Square = 2,
+        Curly = 4
     }
 
-    /// <summary>
-    /// Searches the <see cref="text"/> and returns the list of start and end positions of bracket pairs.
-    /// </summary>
-    /// <param name="text">The source text.</param>
-    /// <returns>The list of start and end positions of bracket pairs.</returns>
-    /// <exception cref="ArgumentNullException"><see cref="text"/> is null.</exception>
-    public static IList<(int Start, int End)> GetBracketPairPositions(this string? text)
+    public static class StringExtensions
     {
-        // TODO #2. Analyze the method unit tests and add the method implementation.
-        throw new NotImplementedException();
-    }
+        // 1. Use Stack for efficient pair counting.
+        public static int CountBracketPairs(this string text)
+        {
+            if (text == null)
+            {
+                return 0;
+            }
 
-    /// <summary>
-    /// Examines the <see cref="text"/> and returns true if the pairs and the orders of brackets are balanced; otherwise returns false.
-    /// </summary>
-    /// <param name="text">The source text.</param>
-    /// <param name="bracketTypes">The bracket type option.</param>
-    /// <returns>True if the pairs and the orders of brackets are balanced; otherwise returns false.</returns>
-    /// <exception cref="ArgumentNullException"><see cref="text"/> is null.</exception>
-    public static bool ValidateBrackets(this string? text, BracketTypes bracketTypes)
-    {
-        // TODO #3. Analyze the method unit tests and add the method implementation.
-        throw new NotImplementedException();
+            var pairs = new Dictionary<char, char> { { '(', ')' }, { '[', ']' } };
+            var stack = new Stack<char>();
+            int count = 0;
+            foreach (var c in text)
+            {
+                if (pairs.ContainsKey(c))
+                {
+                    stack.Push(c);
+                }
+                else if (pairs.ContainsValue(c) && stack.Count > 0 && pairs[stack.Peek()] == c)
+                {
+                    stack.Pop();
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        // 2. Use List to collect tuple positions of all bracket pairs.
+        public static List<(int openIndex, int closeIndex)> GetBracketPairPositions(this string text)
+        {
+            var pairs = new Dictionary<char, char> { { '(', ')' }, { '[', ']' }, { '{', '}' } };
+            var stack = new Stack<(char bracket, int index)>();
+            var result = new List<(int, int)>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (pairs.ContainsKey(c))
+                {
+                    stack.Push((c, i));
+                }
+                else if (pairs.ContainsValue(c))
+                {
+                    if (stack.Count > 0 && pairs.TryGetValue(stack.Peek().bracket, out var expected) && expected == c)
+                    {
+                        var (openBracket, openIndex) = stack.Pop();
+                        result.Add((openIndex, i));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        // 3. Use HashSet for fast bracket lookup; validate as per bracketTypes flags.
+        public static bool ValidateBrackets(this string text, BracketType bracketTypes)
+        {
+            var typePairs = new Dictionary<BracketType, (char open, char close)>
+            {
+                { BracketType.Round, ('(', ')') },
+                { BracketType.Square, ('[', ']') },
+                { BracketType.Curly, ('{', '}') }
+            };
+
+            var bracketStack = new Stack<char>();
+            var openSet = new HashSet<char>();
+            var closeSet = new HashSet<char>();
+            foreach (var type in typePairs.Keys)
+            {
+                if (bracketTypes.HasFlag(type))
+                {
+                    openSet.Add(typePairs[type].open);
+                    closeSet.Add(typePairs[type].close);
+                }
+            }
+
+            foreach (var c in text)
+            {
+                if (openSet.Contains(c))
+                {
+                    bracketStack.Push(c);
+                }
+                else if (closeSet.Contains(c))
+                {
+                    if (bracketStack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char open = bracketStack.Pop();
+                    var expected = typePairs[bracketTypes].open == open ? typePairs[bracketTypes].close : '\0';
+                    if (!typePairs.Values.Any(pair => pair.open == open && pair.close == c))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return bracketStack.Count == 0;
+        }
     }
 }
